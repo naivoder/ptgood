5e4ec1b1d32768c36b054a5a2b7fe9841940af4c
naivoder <naivoder@gmail.com>
2025-03-21 13:53:59 -0400

    latest version of convenience scripts for running the pipeline

--- offline.sh
+++ offline.sh
@@ -1,14 +1,14 @@
 python3 main.py \
   --wandb_key wandb.txt \
-  --env Hopper-v5 \
+  --env Walker2d-v5 \
   --offline_steps 50000 \
   --online_steps 0 \
   --horizon 5 \
-  --critic_norm \
   --model_train_freq 1000 \
   --imagination_freq 1000 \
   --rl_updates_per 20 \
   --rollout_batch_size 100000 \
   --eval_model \
   --save_rl_post_offline 
+  # --critic_norm \
 
--- online.sh
+++ online.sh
@@ -1,17 +1,16 @@
 python3 main.py \
   --wandb_key wandb.txt \
-  --env Hopper-v5 \
+  --env Walker2d-v5 \
   --offline_steps 0 \
   --online_steps 50000 \
   --horizon 5 \
   --r 0.5 \
-  --critic_norm \
   --model_train_freq 1000 \
   --imagination_freq 1000 \
   --rl_updates_per 20 \
   --rollout_batch_size 100000 \
-  --model_file ./models/Hopper-v5.pt \
-  --rl_file ./policies/Hopper-v5 \
+  --model_file ./models/Walker2d-v5.pt \
+  --rl_file ./policies/Walker2d-v5 \
   --ceb_planner \
   --ceb_planner_noise 0.15 \
   --ceb_width 10 \
@@ -19,6 +18,8 @@ python3 main.py \
   --act_ceb_pct 1.1 \
   --ceb_beta 0.01 \
   --ceb_z_dim 8 \
-  --ceb_update_freq 10000000 \
   --learned_marginal \
-  --ceb_file ./ceb_weights/Hopper-v5 
+  --ceb_file ./ceb_weights/Walker2d-v5 
+
+  # --ceb_update_freq 10000000 \
+  # --critic_norm \
